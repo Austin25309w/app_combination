51d1de9426e6ed8bca2af58025f03a5b92453c72
Austin25309w <austinow@gmail.com>
2020-04-18 17:58:39 -0700

    destructuring veriables

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,45 +1,19 @@
 import React from 'react';
+import Exercise from './Exercise'
+import Clock from './Clock'
 
-const list = [
-    {
-        title: 'React',
-        url: 'https://facebook.github.io/react/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    }
-]
 class News extends React.Component {
 
 
-    constructor(firstname, lastname) {
-        super();
-        this.state = {
-            list: list,
-        };
-    }
-
-    getName(){
-        return this.firstname + ' ' + this.lastname;
-    }
-
-    render(){
-        return (
-            <div className="App">
-                {this.state.list.map(item =>
-                    <div key={item.objectID}>
-                        <span>
-                            <a href = {item.url}>{item.title}</a>
-                        </span>
-                        <span>{item.author}</span>|
-                        <span>{item.num_comments}</span>|
-                        <span>{item.points}</span>
-                    </div>
-                    )}
-            </div>
-        )
-    }
+  render(){
+      return (
+          <div>
+            <h1>News</h1>
+              <Exercise/>
+              <Clock />
+          </div>
+      )
+  }
 }
 
 export default News
