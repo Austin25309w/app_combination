797fb082c107c3da308c5964b52504207c40f41a
Austin25309w <austinow@gmail.com>
2020-04-19 13:44:38 -0700

    resusable components

--- src/components/News/Exercise.js
+++ src/components/News/Exercise.js
@@ -67,35 +67,44 @@ class Exercise extends React.Component {
                     value= {searchTerm}
                     onChange={this.onSearchChange }/>
                 </form>
-                { this.state.list.filter(isSearched(this.state.searchTerm)).map(item => )}
-
-                
+               
             </div>
         )
     }
 }
 
-class Search extends Component {
-    render(){
-        const { value, onChange, children } = this.props;
-        return (
-            <form>
-                {children}<input 
-                    type = "text"
-                    value = {value}
-                    onChange={onChange}
-                />
-            </form>
-        );
-    }
-}
+// class Search extends Component {
+//     render(){
+//         const { value, onChange, children } = this.props;
+//         return (
+//             <form>
+//                 {children} <input 
+//                     type = "text"
+//                     value = {value}
+//                     onChange={onChange}
+//                 />
+//             </form>
+//         );
+//     }
+// }
+//ES6
+ const Search = ({value, onChange, children}) =>
+        <form>
+            {children} <input
+                type = "text"
+                value = {value}
+                onChange={onChange}
+            />
+        </form>
+
+
 
 class Table extends Component {
     render(){
         const { list, pattern, onDismiss } = this.props;
         return (
             <div>
-                {this.state.list.map(item =>
+                {list.filter(isSearched(pattern)).map(item =>
                     <div key={item.objectID}>
                         <span>
                             <a href = {item.url}>{item.title}</a>
@@ -105,16 +114,34 @@ class Table extends Component {
                         <span>{item.points}</span>
 
                         <span>
-                            <button
-                                onClick={()=> this.onDismiss(item.objectID)}
-                                type = "button"
-                            >
-                            Dismiss
-                            </button>
+                            <Button onClick={() => onDismiss(item.objectID)}>
+                                Dismiss
+                            </Button>
                         </span>
                     </div>
                 )}
             </div>
+        )
+    }
+}
+
+class Button extends Component {
+    render() {
+        const {
+            onClick,
+            className = '',
+            children,
+        } = this.props
+    
+
+        return (
+            <button
+                onClick= {onClick}
+                className={className}
+                type="button"
+            >
+                {children}
+            </button>
         )
     }
 }
